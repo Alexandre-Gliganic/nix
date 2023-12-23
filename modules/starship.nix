05d43a0aed8e178a79d0ebf92127fcdff137c41c
Alexandre Gliganic <alexandre.gliganic@epita.fr>
2023-12-23 23:07:57 +0100

    chore(starship): add os icon

--- modules/starship.nix
+++ modules/starship.nix
@@ -6,6 +6,19 @@
       battery = {
         disabled = true;
       };
+      kubernetes = {
+        disabled = false;
+        detect_folders = [ "k8s" ];
+      };
+      os = {
+        disabled = false;
+        symbols = {
+          Linux = " ";
+          Windows = " ";
+          Macos = " ";
+          NixOS = " ";
+        };
+      };
     };
   };
 }
