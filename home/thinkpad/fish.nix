fa03ffb8bfc6753aa45730ddaf180fbd2049a30a
Alexandre Gliganic <alexandre.gliganic@epita.fr>
2023-12-23 22:40:11 +0100

    chore: change fish with wraps command

--- home/thinkpad/fish.nix
+++ home/thinkpad/fish.nix
@@ -2,8 +2,9 @@
 {
   programs.fish = {
     functions = {
-      ssh = { description = "ssh to a host with kitty"; body = " kitty +kitten ssh $argv "; };
-      icat = { description = "print image in terminal"; body = " kitty +kitten icat $argv "; };
+      clipboard = { description = "Copy to clipboard"; body = "xsel -ib"; wraps = "xsel"; };
+      icat = { description = "print image in terminal"; body = " kitty +kitten icat $argv "; wraps = "icat"; };
+      ssh = { description = "ssh to a host with kitty"; body = " kitty +kitten ssh $argv "; wraps = "ssh"; };
     };
   };
 }
