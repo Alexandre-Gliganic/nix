cb3eac4aff869bde510e6865dc94f24e3524459b
Alexandre Gliganic <alexandre.gliganic@epita.fr>
2024-02-29 13:59:11 +0100

    chore(thinkpad): add vpn fish function

--- home/thinkpad/fish.nix
+++ home/thinkpad/fish.nix
@@ -5,6 +5,7 @@
       clipboard = { description = "Copy to clipboard"; body = "xsel -ib"; wraps = "xsel"; };
       icat = { description = "print image in terminal"; body = " kitty +kitten icat $argv "; wraps = "icat"; };
       ssh = { description = "ssh to a host with kitty"; body = " kitty +kitten ssh $argv "; wraps = "ssh"; };
+      vpn = { description = "Connect or disconnect to VPN"; body = "nmcli connection $argv[1] $argv[2]"; wraps = "nmcli connection $argv[1]"; };
     };
   };
 }
